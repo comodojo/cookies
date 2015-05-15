eb6cd292f876ca941596f668523bd23ff161c55d
comodojo <info@comodojo.org>
2015-05-15 12:13:21 +0200

    apache port to 8000

--- tests/runme_before_53.sh
+++ tests/runme_before_53.sh
@@ -9,4 +9,5 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php
 sudo cp -f $TRAVIS_BUILD_DIR/build/travis-ci-apache /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
 echo 'date.timezone = "Europe/Rome"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+echo 'Listen 8000' > /etc/apache2/ports.conf
 sudo service apache2 restart
